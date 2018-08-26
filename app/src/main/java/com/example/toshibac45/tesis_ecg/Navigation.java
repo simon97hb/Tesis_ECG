fceb682f95e7c63085aa55c0cd091af30a8ca251
Toshiba_Simón <shernandez97@hotmail.com>
2018-08-26 02:11:03 -0400

    Made Home Layout

--- app/src/main/java/com/example/toshibac45/tesis_ecg/Navigation.java
+++ app/src/main/java/com/example/toshibac45/tesis_ecg/Navigation.java
@@ -5,11 +5,18 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.gigamole.library.PulseView;
 
 public class Navigation extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private PulseView pulseView;
+    private Button toggleBtn;
+    private boolean toggleState = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,31 @@ public class Navigation extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toggleBtn = (Button) findViewById(R.id.toggle_btn);
+        toggleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggle();
+
+            }
+        });
+
+        pulseView = (PulseView) findViewById(R.id.heart_pulse_view);
+
+
+    }
+
+    private void toggle(){
+
+        if (!toggleState){
+            toggleState = true;
+            pulseView.startPulse();
+        } else {
+            toggleState = false;
+            pulseView.finishPulse();
+        }
+
     }
 
 
